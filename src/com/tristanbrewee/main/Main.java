2fc845154bedcfc2c2a91681b5b81def1e3b1cff
tristan <tristanbrewee@gmail.com>
2025-06-16 13:12:15 +0200

    - Set up the project - Added the fields for City.java

---
+++ src/com/tristanbrewee/main/Main.java
@@ -0,0 +1,8 @@
+package com.tristanbrewee.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        }
+}
---
+++ src/com/tristanbrewee/model/City.java
@@ -0,0 +1,10 @@
+package com.tristanbrewee.model;
+
+public class City {
+
+    private int id;
+    private String name;
+    private String countryId;
+    private String district;
+    private int population;
+}
